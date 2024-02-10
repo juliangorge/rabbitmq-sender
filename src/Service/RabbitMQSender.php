9219cf1c679b4b0ab55175002e224406e9ff2191
juliangorge-lrn <juliangorge@hotmail.com>
2024-02-10 18:57:29 -0300

    Added Service

--- src/Service/RabbitMQSender.php
+++ src/Service/RabbitMQSender.php
@@ -3,5 +3,14 @@ namespace Juliangorge\RabbitMQSender\Service;
 
 class RabbitMQSender
 {
+    protected $sender;
+
+    public function __construct($sm){
+        $this->sender = new \Juliangorge\RabbitMQSender\RabbitMQSender($sm);
+    }
+
+    public function send($queue, $message){
+        return $this->sender->send($queue, $message);
+    }
 
 }
